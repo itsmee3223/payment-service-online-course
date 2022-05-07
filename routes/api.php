ba462825e9a001e16c8035aa36c7fa44ebd046e8
itsmee3223 <tamvam.pemberani@gmail.com>
2022-05-07 20:00:11 +0700

    add route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\orderController;
+use App\Http\Controllers\webhookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +15,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('orders', [orderController::class, 'index']);
+Route::post('orders', [orderController::class, 'create']);
+
+Route::post('webhook', [webhookController::class, 'midtransHandler']);
